e78ba4fe5d224fb2567bca2d092e10e57bc5e0f7
Paul Thomson <paulthomson@users.noreply.github.com>
2019-10-23 17:27:10 +0100

    Add build workflow (#1)

---
+++ build.sh
@@ -0,0 +1,244 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+set -e
+set -u
+
+WORK="$(pwd)"
+
+# Old bash versions can't expand empty arrays, so we always include at least this option.
+CMAKE_OPTIONS=("-DCMAKE_OSX_ARCHITECTURES=x86_64")
+
+uname
+
+case "$(uname)" in
+"Linux")
+  GH_RELEASE_TOOL_ARCH="linux_amd64"
+  NINJA_OS="linux"
+  BUILD_PLATFORM="Linux_x64"
+  PYTHON="python3"
+  ;;
+
+"Darwin")
+  GH_RELEASE_TOOL_ARCH="darwin_amd64"
+  NINJA_OS="mac"
+  BUILD_PLATFORM="Mac_x64"
+  PYTHON="python3"
+  brew install md5sha1sum
+  ;;
+
+"MINGW"*)
+  GH_RELEASE_TOOL_ARCH="windows_amd64"
+  NINJA_OS="win"
+  BUILD_PLATFORM="Windows_x64"
+  PYTHON="python"
+  CMAKE_OPTIONS+=("-DCMAKE_C_COMPILER=cl.exe" "-DCMAKE_CXX_COMPILER=cl.exe")
+  choco install zip
+  ;;
+
+*)
+  echo "Unknown OS"
+  exit 1
+  ;;
+esac
+
+###### START EDIT ######
+TARGET_REPO_ORG="swiftshader"
+TARGET_REPO_NAME="SwiftShader"
+BUILD_REPO_ORG="google"
+BUILD_REPO_NAME="gfbuild-swiftshader"
+###### END EDIT ######
+
+COMMIT_ID="$(cat "${WORK}/COMMIT_ID")"
+
+ARTIFACT="${BUILD_REPO_NAME}"
+ARTIFACT_VERSION="${COMMIT_ID}"
+GROUP_DOTS="github.${BUILD_REPO_ORG}"
+GROUP_SLASHES="github/${BUILD_REPO_ORG}"
+TAG="${GROUP_SLASHES}/${ARTIFACT}/${ARTIFACT_VERSION}"
+
+BUILD_REPO_SHA="${GITHUB_SHA}"
+CLASSIFIER="${BUILD_PLATFORM}_${CONFIG}"
+POM_FILE="${BUILD_REPO_NAME}-${ARTIFACT_VERSION}.pom"
+INSTALL_DIR="${ARTIFACT}-${ARTIFACT_VERSION}-${CLASSIFIER}"
+
+GH_RELEASE_TOOL_USER="c4milo"
+GH_RELEASE_TOOL_VERSION="v1.1.0"
+
+export PATH="${HOME}/bin:$PATH"
+
+mkdir -p "${HOME}/bin"
+
+pushd "${HOME}/bin"
+
+# Install github-release.
+curl -fsSL -o github-release.tar.gz "https://github.com/${GH_RELEASE_TOOL_USER}/github-release/releases/download/${GH_RELEASE_TOOL_VERSION}/github-release_${GH_RELEASE_TOOL_VERSION}_${GH_RELEASE_TOOL_ARCH}.tar.gz"
+tar xf github-release.tar.gz
+
+# Install ninja.
+curl -fsSL -o ninja-build.zip "https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-${NINJA_OS}.zip"
+unzip ninja-build.zip
+
+ls
+
+popd
+
+###### START EDIT ######
+CMAKE_GENERATOR="Ninja"
+CMAKE_BUILD_TYPE="${CONFIG}"
+CMAKE_OPTIONS+=("-DBUILD_EGL=0" "-DBUILD_GLESv2=0" "-DBUILD_GLES_CM=0" "-DBUILD_SAMPLES=0" "-DBUILD_TESTS=0" "-DWARNINGS_AS_ERRORS=0")
+
+git clone --recurse-submodules "https://${TARGET_REPO_ORG}.googlesource.com/${TARGET_REPO_NAME}" "${TARGET_REPO_NAME}"
+cd "${TARGET_REPO_NAME}"
+git checkout "${COMMIT_ID}"
+###### END EDIT ######
+
+###### BEGIN BUILD ######
+BUILD_DIR="b_${CONFIG}"
+
+mkdir -p "${BUILD_DIR}"
+pushd "${BUILD_DIR}"
+
+cmake -G "${CMAKE_GENERATOR}" .. "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" "${CMAKE_OPTIONS[@]}"
+cmake --build . --config "${CMAKE_BUILD_TYPE}"
+# There is no install step for SwiftShader, but there is for third party libraries, so skip this step.
+#cmake "-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}" "-DBUILD_TYPE=${CMAKE_BUILD_TYPE}" -P cmake_install.cmake
+popd
+###### END BUILD ######
+
+###### START EDIT ######
+
+# There is no install step for SwiftShader, so copy files manually.
+
+mkdir -p "${INSTALL_DIR}/lib"
+
+case "$(uname)" in
+"Linux")
+  SWIFT_SHADER_PLATFORM_OUTPUT="Linux"
+  ;;
+
+"Darwin")
+  SWIFT_SHADER_PLATFORM_OUTPUT="Darwin"
+  ;;
+
+"MINGW"*)
+  SWIFT_SHADER_PLATFORM_OUTPUT="Windows"
+  ;;
+
+*)
+  echo "Unknown OS"
+  exit 1
+  ;;
+esac
+
+# Copy the contents of the output directory into lib.
+cp -r "${BUILD_DIR}/${SWIFT_SHADER_PLATFORM_OUTPUT}/." "${INSTALL_DIR}/lib/"
+
+# Add .pdb files on Windows.
+case "$(uname)" in
+"Linux")
+  ;;
+
+"Darwin")
+  ;;
+
+"MINGW"*)
+  "${PYTHON}" "${WORK}/add_pdbs.py" "${BUILD_DIR}" "${INSTALL_DIR}"
+  ;;
+
+*)
+  echo "Unknown OS"
+  exit 1
+  ;;
+esac
+
+for f in "${INSTALL_DIR}/lib/"*; do
+  echo "${BUILD_REPO_SHA}">"${f}.build-version"
+  cp "${WORK}/COMMIT_ID" "${f}.version"
+done
+###### END EDIT ######
+
+GRAPHICSFUZZ_COMMIT_SHA="b82cf495af1dea454218a332b88d2d309657594d"
+OPEN_SOURCE_LICENSES_URL="https://github.com/google/gfbuild-graphicsfuzz/releases/download/github/google/gfbuild-graphicsfuzz/${GRAPHICSFUZZ_COMMIT_SHA}/OPEN_SOURCE_LICENSES.TXT"
+
+# Add licenses file.
+curl -fsSL -o OPEN_SOURCE_LICENSES.TXT "${OPEN_SOURCE_LICENSES_URL}"
+cp OPEN_SOURCE_LICENSES.TXT "${INSTALL_DIR}/"
+
+# zip file.
+pushd "${INSTALL_DIR}"
+zip -r "../${INSTALL_DIR}.zip" ./*
+popd
+
+sha1sum "${INSTALL_DIR}.zip" >"${INSTALL_DIR}.zip.sha1"
+
+# POM file.
+sed -e "s/@GROUP@/${GROUP_DOTS}/g" -e "s/@ARTIFACT@/${ARTIFACT}/g" -e "s/@VERSION@/${ARTIFACT_VERSION}/g" "../fake_pom.xml" >"${POM_FILE}"
+
+sha1sum "${POM_FILE}" >"${POM_FILE}.sha1"
+
+DESCRIPTION="$(echo -e "Automated build for ${TARGET_REPO_NAME} version ${COMMIT_ID}.\n$(git log --graph -n 3 --abbrev-commit --pretty='format:%h - %s <%an>')")"
+
+# Only release from master branch commits.
+# shellcheck disable=SC2153
+if test "${GITHUB_REF}" != "refs/heads/master"; then
+  exit 0
+fi
+
+# We do not use the GITHUB_TOKEN provided by GitHub Actions.
+# We cannot set enviroment variables or secrets that start with GITHUB_ in .yml files,
+# but the github-release tool requires GITHUB_TOKEN, so we set it here.
+export GITHUB_TOKEN="${GH_TOKEN}"
+
+github-release \
+  "${BUILD_REPO_ORG}/${BUILD_REPO_NAME}" \
+  "${TAG}" \
+  "${BUILD_REPO_SHA}" \
+  "${DESCRIPTION}" \
+  "${INSTALL_DIR}.zip"
+
+github-release \
+  "${BUILD_REPO_ORG}/${BUILD_REPO_NAME}" \
+  "${TAG}" \
+  "${BUILD_REPO_SHA}" \
+  "${DESCRIPTION}" \
+  "${INSTALL_DIR}.zip.sha1"
+
+# Don't fail if pom cannot be uploaded, as it might already be there.
+
+github-release \
+  "${BUILD_REPO_ORG}/${BUILD_REPO_NAME}" \
+  "${TAG}" \
+  "${BUILD_REPO_SHA}" \
+  "${DESCRIPTION}" \
+  "${POM_FILE}" || true
+
+github-release \
+  "${BUILD_REPO_ORG}/${BUILD_REPO_NAME}" \
+  "${TAG}" \
+  "${BUILD_REPO_SHA}" \
+  "${DESCRIPTION}" \
+  "${POM_FILE}.sha1" || true
+
+# Don't fail if OPEN_SOURCE_LICENSES.TXT cannot be uploaded, as it might already be there.
+
+github-release \
+  "${BUILD_REPO_ORG}/${BUILD_REPO_NAME}" \
+  "${TAG}" \
+  "${BUILD_REPO_SHA}" \
+  "${DESCRIPTION}" \
+  "OPEN_SOURCE_LICENSES.TXT" || true
